b17254205f7e58765607a64f34c1da7d1accefd2
tatsuei <eiji@tutanota.com>
2021-01-26 21:47:51 +0800

    Added char-matching regex code, and loop to restart program.

--- chapter4/c4allMyCats2.py
+++ chapter4/c4allMyCats2.py
@@ -1,11 +1,49 @@
-floofNames = []
-while True:
-    print('Hey! Give this smol floof a name?', str(len(floofNames) + 1),
-          ' (Or you could just press enter to skip this.):')
-    name = input()
-    if name == '':
-        break
-    floofNames = catNames + [name]
-print('So, our floofs are called:')
-for name in floofNames:
-    print('   ' + name)
+import re   # Regular expression library
+import sys
+
+# Checking user input to see if they wanna restart the program
+def checkRes():
+    noNames = input('Aww. They don\'t have names yet. Wanna name them?'
+                    ' (Y/N)\n')
+    return noNames
+
+def floofNaming():
+    floofNames = []
+    while True:
+        print('Hello! Give this smol floof a name?', 'Floof #' + 
+              str(len(floofNames) + 1), '(Or press enter to skip.):')
+
+        name = input()
+        if name == '':
+            break
+        elif not re.match("^[a-z]*$", name, flags=re.I): 
+            print('Hey! Names begin with letters! L E T T E R S!')
+            continue
+
+        floofNames = floofNames + [name]
+
+    # Checks for empty array using Boolean logic, ie fN != fN == null/None
+    if not floofNames:
+        while True:
+            userRes = checkRes()
+            if userRes.lower() == 'y':
+                floofNaming()
+            elif userRes.lower() == 'n':
+                print('Okay :( You should give them names next time! :D')
+                sys.exit()
+            else:
+                continue
+    else:
+        print('So, our floofs are called:')
+
+    for name in floofNames:
+        print('   ' + name)
+
+floofNaming()
+
+# elif not re.match("^[a-z]*$", name, flags=re.I): 
+# Regex checking for letters, 're.I' where I = ignorecase.
+# expression meaning: 'if any (*) character at the start (^)
+# or end ($) doesn't match letters between a-z (represented
+# by [a-z] with the square brackets meaning the set of
+# characters to match
